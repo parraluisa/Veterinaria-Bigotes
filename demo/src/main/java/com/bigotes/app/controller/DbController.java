72bc31f3c100a3daa72bf6d5ad47c9a369d1db26
parraluisa <parran_ll@javeriana.edu.co>
2023-10-01 22:27:06 -0500

    info updated on Db COntroller

--- demo/src/main/java/com/bigotes/app/controller/DbController.java
+++ demo/src/main/java/com/bigotes/app/controller/DbController.java
@@ -130,7 +130,7 @@ public class DbController implements ApplicationRunner {
         // Crear mascota 1
         petRepository.save(new Pet("Pompita", "British Shorthair", LocalDate.of(2015, 2, 12), 824.65, "Hipotiroidismo", "https://catinaflat.blog/wp-content/uploads/2022/03/british-shorthair-1.jpg"));
         // Crear mascota 2
-        petRepository.save(new Pet("Fifi", "Abisinio", LocalDate.of(2023, 7, 24), 200.00, "Tumores mamarios", "https://images.pexels.com/photos/866496/pexels-photo-866496.jpeg?auto=compress&cs=tinysrgb&w=600"));
+        petRepository.save(new Pet("Fifi", "Abisinio", LocalDate.of(2023, 7, 24), 200.00, "rinotraqueítis felina", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.vBdcyqdY0z6igk5tfrflTQHaHa%26pid%3DApi&f=1&ipt=9a4ae16ea2d8f1a780e82c8a1ccbdbd022e6cfd3d22a02b712097477650fd6ac&ipo=images"));
         // Crear mascota 3
         petRepository.save(new Pet("Mimosa", "Maine Coon", LocalDate.of(2020, 4, 15), 4065.82, "Conjuntivitis", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Dg-lJLyICJTPKLDcKOuYtgHaEo%26pid%3DApi&f=1&ipt=74cb2a44995bd708e41b783acc38bbe08949fdf0f140e082347bd5ab5efa6dd6&ipo=images"));
         // Crear mascota 4
@@ -636,7 +636,7 @@ public class DbController implements ApplicationRunner {
         // Create and save drug 1
         drugRepository.save(new Drug("Felimazole", 150000.0));
         // Create and save drug 2
-        drugRepository.save(new Drug("FurVitality", 120000.0));
+        drugRepository.save(new Drug("Famciclovir", 120000.0));
         // Create and save drug 3
         drugRepository.save(new Drug("PawPain Away", 190000.0));
         // Create and save drug 4
@@ -678,7 +678,7 @@ public class DbController implements ApplicationRunner {
         // Create and save treatment 1
         treatmentRepository.save(new Treatment(LocalDate.of(2023, 9, 20),  "Se le dió al felino el primer comprimido de FELIMAZOLE, revisión dentro de 12 horas."));
         // Create and save treatment 2
-        treatmentRepository.save(new Treatment(LocalDate.of(2020, 3, 15), "Se administró FurVitality al gato para mejorar su pelaje y vitalidad. Se realizará un seguimiento en 24 horas.") );
+        treatmentRepository.save(new Treatment(LocalDate.of(2023, 9, 28), "Se administró Famciclovir para tratar la rinotraqueítis felina. Se realizará un seguimiento en 24 horas.") );
         // Create and save treatment 3
         treatmentRepository.save(new Treatment(LocalDate.of(2021, 5, 10), "Se aplicó PawPain Away al gato para aliviar el dolor en las patas. Revisión dentro de 12 horas para evaluar el alivio del dolor."));
         // Create and save treatment 4
