a4547a582246cea0574e387af90bfa1c52df366c
FabioB8a <fabiobuitra@gmail.com>
2023-10-15 16:29:40 -0500

    FINISH: Drug Controller

--- demo/src/main/java/com/bigotes/app/controller/DrugController.java
+++ demo/src/main/java/com/bigotes/app/controller/DrugController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +26,33 @@ public class DrugController {
 
     // http://localhost:8090/drug
     @GetMapping()
-    public List<Drug> showAllOwners() {
+    public List<Drug> showAllDrugs() {
         return drugService.findAll();
     }
+
+    // http://localhost:8090/drug/{id}
+    @GetMapping("/{id}")
+    public Drug showDrug(@PathVariable("id") Long id) {
+        return drugService.findById(id);
+    }
+
+    // http://localhost:8090/drug
+    @PostMapping()
+    public void insertDrug(@RequestBody Drug drug) {
+        drugService.save(drug);
+    }
+
+    // http://localhost:8090/drug
+    @PutMapping()
+    public void updateDrug(@RequestBody Drug drug) {
+        drugService.save(drug);
+    }
+
+    // http://localhost:8090/drug/{id}
+    @DeleteMapping("/{id}")
+    public void deleteDrug(@PathVariable("id") Long id) {
+        drugService.deleteById(id);
+    }
+
 
 }
