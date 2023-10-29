4db0eece88f3e0bbad721705b02d997cf70c7ad4
FabioB8a <fabiobuitra@gmail.com>
2023-10-29 16:09:50 -0500

    update: first use case

--- demo/src/test/java/com/bigotes/app/e2e/RegisterVet_UseCaseTest.java
+++ demo/src/test/java/com/bigotes/app/e2e/RegisterVet_UseCaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
@@ -45,6 +46,12 @@ public class RegisterVet_UseCaseTest {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+
     // Descripción: Llega un usuario nuevo a la veterinaria con su mascota. El
     // veterinario que está disponible en ese momento intenta ingresar con su usuario
     // y contraseña al sistema. Este se equivoca la primera vez, sin embargo, al segundo
@@ -60,6 +67,170 @@ public class RegisterVet_UseCaseTest {
     @Test
     public void vet_Use_Case(){
 
+        // Arrange
+        // Ingreso de Veterinario
+        vetLogin();
+
+        // Va a la sección de registro de clientes
+        // Ir a dashboard clientes
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
+
+        // -> LISTA INICIAL: Pet
+        List<WebElement> listaInicialPet = driver.findElements(By.className("petImage"));
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAllOwner")));
+        WebElement btnAllOwner = driver.findElement(By.id("btnAllOwner"));
+        btnAllOwner.click();
+        
+        // Oprimir en botón: agregar cliente
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("ownerId")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnsaveOwner")));
+
+        // -> LISTA INICIAL: Owner
+        List<WebElement> listaIncialOwner = driver.findElements(By.className("ownerId"));
+
+        WebElement btnSaveOwner = driver.findElement(By.id("btnsaveOwner"));
+        btnSaveOwner.click();
+
+        // Act
+        // pide los datos y da en el botón de “registrar”
+        WebElement inputIDCardOwner = driver.findElement(By.id("idCard"));
+        WebElement inputNameOwner = driver.findElement(By.id("firstName"));
+        WebElement inputFLNOwner = driver.findElement(By.id("firstLastName"));
+        WebElement inputSLNOwner = driver.findElement(By.id("secondLastName"));
+        WebElement inputPhoneOwner = driver.findElement(By.id("phone"));
+        WebElement inputEmailOwner = driver.findElement(By.id("email"));
+
+        inputIDCardOwner.sendKeys("1001298924");
+        inputNameOwner.sendKeys("Fabio");
+        inputFLNOwner.sendKeys("Luis");
+        inputSLNOwner.sendKeys("Buitrago");
+
+        // -> Teléfono incorrecto
+        inputPhoneOwner.sendKeys("TeléfonoIncorrecto");
+        inputEmailOwner.sendKeys("fabiobuitra@gmail.com");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveOwnerForm")));
+        WebElement btnSaveOwnerForm = driver.findElement(By.id("btnSaveOwnerForm"));
+        btnSaveOwnerForm.click();
+
+        // Nuevamente el veterinario se equivoca en uno de los campos, corrige e intenta nuevamente.
+
+        // -> Control de alerta
+        // Espera a que aparezca la alerta.
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        // Cambia el control al objeto Alert.
+        Alert alert = driver.switchTo().alert();
+
+        // Acepta la alerta haciendo clic en el botón "Aceptar".
+        alert.accept();
+
+        inputPhoneOwner.clear();
+        inputPhoneOwner.sendKeys("3124606315");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveOwnerForm")));
+        btnSaveOwnerForm.click();
+
+        // Posteriormente va a la sección de mascotas
+
+        // -> Ir a DashBoard de mascotas
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("ownerId")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAllPets")));
+
+        // -> LISTA FINAL: Owner
+        List<WebElement> listaFinalOwner = driver.findElements(By.className("ownerId"));
+
+        WebElement btnAllPets = driver.findElement(By.id("btnAllPets"));
+        btnAllPets.click();
+        
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAddPet")));
+
+        WebElement btnAddPet = driver.findElement(By.id("btnAddPet"));
+        btnAddPet.click();
+
+        // y registra la mascota asociándola al dueño. Esta vez todo sale bien a la primera.
+        WebElement inputIDCardOwnerPet = driver.findElement(By.id("ownerId"));
+        WebElement inputNamePet= driver.findElement(By.id("name"));
+        WebElement inputBreedPet = driver.findElement(By.id("breed"));
+        WebElement inputBirthdayPet = driver.findElement(By.id("birthday"));
+        WebElement inputWeightPet = driver.findElement(By.id("weight"));
+        WebElement inputDiseasePet = driver.findElement(By.id("disease"));
+        WebElement inputImgUrlPet = driver.findElement(By.id("imgUrl"));
+
+        String expectedNamePet = "Maw";
+        String expectedBreedPet = "Gato naranja";
+        String expectedAgePet = "3";
+        String expectedOwnerName = "Fabio Luis";
+        String expectedWeightPet = "500";
+        String expectedDiseasePet = "Tos leve";
+        String expectedImgUrlPet = "https://catvills.com/wp-content/uploads/2020/07/orange-and-white-cat-names-a.jpg";
+
+        inputIDCardOwnerPet.sendKeys("1001298924");
+        inputBirthdayPet.sendKeys("23/07/2020");
+        inputNamePet.sendKeys(expectedNamePet);
+        inputBreedPet.sendKeys(expectedBreedPet);
+        inputWeightPet.sendKeys(expectedWeightPet);
+        inputDiseasePet.sendKeys(expectedDiseasePet);
+        inputImgUrlPet.sendKeys(expectedImgUrlPet);
+
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAddPetForm")));
+        WebElement btnAddPetForm = driver.findElement(By.id("btnAddPetForm"));
+        btnAddPetForm.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
+
+        // -> LISTA FINAL: Pet
+        List<WebElement> listaFinalPet = driver.findElements(By.className("petImage"));
+
+        // El veterinario pide al dueño que ingrese con su cedula a través del portal de clientes
+        userLogin();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
+
+        List<WebElement> listaFinalPetOwner = driver.findElements(By.className("pet-detail"));
+        
+        // Obtener el primer elemento de la lista
+        WebElement primeraMascota = listaFinalPetOwner.get(0);
+        primeraMascota.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pet")));
+
+        // Assert
+
+        // VERIFICACIÓN: Agregado correcto de Owner
+        Assertions.assertThat(listaFinalOwner.size()).isEqualTo(listaIncialOwner.size() + 1);
+
+        // VERIFICACIÓN: Agregado correcto de Pet
+        Assertions.assertThat(listaFinalPet.size()).isEqualTo(listaInicialPet.size() + 1);
+
+        // VERIFICACIÓN: Agregado correcto de Pet en la vista de Owner
+        Assertions.assertThat(listaFinalPetOwner.size()).isEqualTo(1);
+
+        // VERIFICACIÓN: Datos correctos de la mascota
+        WebElement lblOwnerName = driver.findElement(By.id("pet-owner-str"));
+        Assertions.assertThat(lblOwnerName.getText()).isEqualTo(expectedOwnerName);
+
+        WebElement lblPetName = driver.findElement(By.id("pet-name-str"));
+        Assertions.assertThat(lblPetName.getText()).isEqualTo(expectedNamePet);
+
+        WebElement lblWeightName = driver.findElement(By.id("pet-weight-str"));
+        Assertions.assertThat(lblWeightName.getText()).isEqualTo(expectedWeightPet + " gr");
+
+        WebElement lblPetBreed = driver.findElement(By.id("pet-breed-str"));
+        Assertions.assertThat(lblPetBreed.getText()).isEqualTo(expectedBreedPet);
+
+        WebElement lblPetAge = driver.findElement(By.id("pet-age-str"));
+        Assertions.assertThat(lblPetAge.getText()).isEqualTo(expectedAgePet + " años");
+
+        WebElement lblURLImagetAge = driver.findElement(By.id("pet-img"));
+        Assertions.assertThat(lblURLImagetAge.getAttribute("src")).isEqualTo(expectedImgUrlPet);
+
+    }
+
+    // Login Veterinario
+    private void vetLogin() {
         // Llega un usuario nuevo a la veterinaria con su mascota.
         driver.get(BASE_URL + "/home");
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btn-login")));
@@ -68,7 +239,6 @@ public class RegisterVet_UseCaseTest {
 
         // El veterinario que está disponible en ese momento intenta ingresar con su usuario
         // y contraseña al sistema.
-
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("loginVet")));
         WebElement btnVet = driver.findElement(By.className("loginVet"));
         btnVet.click();
@@ -103,111 +273,10 @@ public class RegisterVet_UseCaseTest {
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnVetSubmit")));
         btnSubmit.click();
+    }
 
-        // Va a la sección de registro de clientes
-
-        // Ir a dashboard clientes
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAllOwner")));
-        WebElement btnAllOwner = driver.findElement(By.id("btnAllOwner"));
-        btnAllOwner.click();
-        
-        // Oprimir en botón: agregar cliente
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("ownerId")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnsaveOwner")));
-
-        // -> LISTA INICIAL: Owner
-        List<WebElement> listaIncialOwner = driver.findElements(By.className("ownerId"));
-
-        WebElement btnSaveOwner = driver.findElement(By.id("btnsaveOwner"));
-        btnSaveOwner.click();
-
-        // pide los datos y da en el botón de “registrar”
-        WebElement inputIDCardOwner = driver.findElement(By.id("idCard"));
-        WebElement inputNameOwner = driver.findElement(By.id("firstName"));
-        WebElement inputFLNOwner = driver.findElement(By.id("firstLastName"));
-        WebElement inputSLNOwner = driver.findElement(By.id("secondLastName"));
-        WebElement inputPhoneOwner = driver.findElement(By.id("phone"));
-        WebElement inputEmailOwner = driver.findElement(By.id("email"));
-
-        inputIDCardOwner.sendKeys("1001298924");
-        inputNameOwner.sendKeys("Fabio");
-        inputFLNOwner.sendKeys("Luis");
-        inputSLNOwner.sendKeys("Buitrago");
-
-        // -> Teléfono incorrecto
-        inputPhoneOwner.sendKeys("TeléfonoIncorrecto");
-        inputEmailOwner.sendKeys("fabiobuitra@gmail.com");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveOwnerForm")));
-        WebElement btnSaveOwnerForm = driver.findElement(By.id("btnSaveOwnerForm"));
-        btnSaveOwnerForm.click();
-
-        // Nuevamente el veterinario se equivoca en uno de los campos, corrige e intenta nuevamente.
-
-        // -> Control de alerta
-        // Espera a que aparezca la alerta.
-        wait.until(ExpectedConditions.alertIsPresent());
-
-        // Cambia el control al objeto Alert.
-        alert = driver.switchTo().alert();
-
-        // Acepta la alerta haciendo clic en el botón "Aceptar".
-        alert.accept();
-
-        inputPhoneOwner.clear();
-        inputPhoneOwner.sendKeys("3124606315");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnSaveOwnerForm")));
-        btnSaveOwnerForm.click();
-
-        // Posteriormente va a la sección de mascotas
-
-        // -> Ir a DashBoard de mascotas
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("ownerId")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAllPets")));
-
-        // -> LISTA FINAL: Owner
-        List<WebElement> listaFinalOwner = driver.findElements(By.className("ownerId"));
-
-        // VERIFICACIÓN: Agregado correcto de Owner
-        Assertions.assertThat(listaFinalOwner.size()).isEqualTo(listaIncialOwner.size() + 1);
-
-        WebElement btnAllPets = driver.findElement(By.id("btnAllPets"));
-        btnAllPets.click();
-        
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAddPet")));
-
-        List<WebElement> listaInicialPet = driver.findElements(By.className("petImage"));
-
-        WebElement btnAddPet = driver.findElement(By.id("btnAddPet"));
-        btnAddPet.click();
-
-        // y registra la mascota asociándola al dueño. Esta vez todo sale bien a la primera.
-        WebElement inputIDCardOwnerPet = driver.findElement(By.id("ownerId"));
-        WebElement inputNamePet= driver.findElement(By.id("name"));
-        WebElement inputBreedPet = driver.findElement(By.id("breed"));
-        WebElement inputBirthdayPet = driver.findElement(By.id("birthday"));
-        WebElement inputWeightPet = driver.findElement(By.id("weight"));
-        WebElement inputDiseasePet = driver.findElement(By.id("disease"));
-        WebElement inputImgUrlPet = driver.findElement(By.id("imgUrl"));
-
-        inputIDCardOwnerPet.sendKeys("1001298924");
-        inputNamePet.sendKeys("Maw");
-        inputBreedPet.sendKeys("Gato naranja");
-        inputBirthdayPet.sendKeys("23/07/2020");
-        inputWeightPet.sendKeys("500");
-        inputDiseasePet.sendKeys("Tos leve");
-        inputImgUrlPet.sendKeys("https://catvills.com/wp-content/uploads/2020/07/orange-and-white-cat-names-a.jpg");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAddPetForm")));
-        WebElement btnAddPetForm = driver.findElement(By.id("btnAddPetForm"));
-        btnAddPetForm.click();
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
-
-        // El veterinario pide al dueño que ingrese con su cedula a través del portal de clientes
+    // Login User
+    private void userLogin(){
 
         driver.get(BASE_URL + "/login/show");
 
@@ -219,8 +288,5 @@ public class RegisterVet_UseCaseTest {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnOwnerSubmit")));
         WebElement btnSubmitOwner = driver.findElement(By.id("btnOwnerSubmit"));
         btnSubmitOwner.click();
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("petImage")));
-
     }
 }
