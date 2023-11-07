7760c3fab61815c049ae7275630044097908a537
andres345gm <andres345gm@gmail.com>
2023-11-06 20:51:43 -0500

    ADD: JWT basic classes

---
+++ demo/src/main/java/com/bigotes/app/security/JWTGenerator.java
@@ -0,0 +1,49 @@
+package com.bigotes.app.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+
+@Component
+public class JWTGenerator {
+
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public static final Long EXPIRATION_TIME = 7000000L;
+
+    /*Crear el JWT */
+    public String generateToken(Authentication authentication) {
+        /*Datos necesarios para la creación */
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + EXPIRATION_TIME);
+
+        /*Crear el JWT */
+        return Jwts.builder().setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    /*Metodo para obtener el usuario a partir del JWT */
+    public String getUserFromJwt(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    /*Metodo para validar el JWT */
+    public boolean validateToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
