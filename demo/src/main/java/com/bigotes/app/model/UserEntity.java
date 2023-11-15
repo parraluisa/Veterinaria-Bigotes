2954fd7930036c2f90c3ae964d0a33b554a97c76
parraluisa <parran_ll@javeriana.edu.co>
2023-11-14 21:45:59 -0500

    Remove unnecessary cascade option in UserEntity ManyToMany relationship

--- demo/src/main/java/com/bigotes/app/model/UserEntity.java
+++ demo/src/main/java/com/bigotes/app/model/UserEntity.java
@@ -22,7 +22,7 @@ public class UserEntity {
     private String password;
 
     /*Siempre que se cargue un usuario se deben cargar los roles */
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     /*No es necesario ponerlo */
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
